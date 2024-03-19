5df9c026d516d8d4e05030d29757ab5a13be85d0
zuzanna0422 <150344209+zuzanna0422@users.noreply.github.com>
2024-03-19 20:35:40 +0100

    Add a bool to the BaseGradeBook constructor

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -16,7 +16,7 @@ namespace GradeBook.GradeBooks
         public List<Student> Students { get; set; }
         public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
