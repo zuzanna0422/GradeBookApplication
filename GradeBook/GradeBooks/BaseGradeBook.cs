284ec69f475d90fb00c685df710d6b0823c42b65
zuzanna0422 <150344209+zuzanna0422@users.noreply.github.com>
2024-03-19 20:37:46 +0100

    Set IsWeight in the BaseGradeBook constructor

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -20,6 +20,7 @@ namespace GradeBook.GradeBooks
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
