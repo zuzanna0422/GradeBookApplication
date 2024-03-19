c8df8917f05ba522481f41ea892498bfb72808ab
zuzanna0422 <150344209+zuzanna0422@users.noreply.github.com>
2024-03-19 20:39:47 +0100

    Add a bool to the StandardGradeBook constructor

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -8,7 +8,7 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(string name) : base(name)
+        public StandardGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
             Type = Enums.GradeBookType.Standard;
         }
