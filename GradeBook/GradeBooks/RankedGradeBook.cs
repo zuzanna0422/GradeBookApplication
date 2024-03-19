97690c1b7e8f0a53b18e0425ad7a2f60ab1b5f73
zuzanna0422 <150344209+zuzanna0422@users.noreply.github.com>
2024-03-19 20:41:04 +0100

    Add a bool to the RankedGradeBook constructor

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -8,7 +8,7 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) : base(name) 
+        public RankedGradeBook(string name, bool IsWeighted) : base(name, IsWeighted) 
         {
             Type = Enums.GradeBookType.Ranked;
         }
