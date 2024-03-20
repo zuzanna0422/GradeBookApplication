fa27f9766a8caba9a38b98c48d7efe56f3ab26ab
zuzanna0422 <150344209+zuzanna0422@users.noreply.github.com>
2024-03-20 18:52:25 +0100

    Override RankedGradeBook's CalculateStudentStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -24,5 +24,17 @@ namespace GradeBook.GradeBooks
                 base.CalculateStatistics();
             }
         }
+        
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
     }
 }
